0210201fa662495c57d8f9c95dbde3d820ec03a4
Bernd Kiefer <bernd.kiefer@dfki.de>
2023-08-22 14:24:09 +0200

    fix start script.

--- run_mosquitto.sh
+++ run_mosquitto.sh
@@ -2,6 +2,6 @@
 scrdir=`dirname $0`
 cd "$scrdir"
 docker container ls | grep -q mqtt-broker ||
-    docker run --name mqtt-broker -d -p1883:1883 -p9001:9001 \
-           -v ./mosquitto.conf:/mosquitto/config/mosquitto.conf:ro \
+    docker run --name mqtt-broker -d -p 1883:1883 -p 9001:9001 \
+           -v `pwd`/mosquitto.conf:/mosquitto/config/mosquitto.conf:ro \
            eclipse-mosquitto:2.0.15
